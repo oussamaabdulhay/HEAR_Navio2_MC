b8a1c126b75d2db9c9dadee6f3dcd8fafe33ca9e
oussamaabdulhay <oussama.abdulhay@gmail.com>
2021-01-19 10:38:56 +0400

    changed the takeoff distance

--- example_node/camera_mrft_node.cpp
+++ example_node/camera_mrft_node.cpp
@@ -26,7 +26,7 @@
 
 
 #define MRFT_Z_CAMERA
-#define MRFT_X_CAMERA
+#undef MRFT_X_CAMERA
 
 
 int main(int argc, char** argv) {
@@ -125,7 +125,7 @@ int main(int argc, char** argv) {
     MissionElement* initial_pose_waypoint = new SetRelativeWaypoint(0., 0., 0., 0.); //TODO: SetRelativeWaypoint needs substantial refactoring
 
    
-    MissionElement* takeoff_relative_waypoint = new SetRelativeWaypoint(0., 0., 1.0, 0.);
+    MissionElement* takeoff_relative_waypoint = new SetRelativeWaypoint(0., 0., 1.5, 0.);
 
     MissionElement* land_relative_waypoint = new SetRelativeWaypoint(0., 0., -2., 0.);
 
