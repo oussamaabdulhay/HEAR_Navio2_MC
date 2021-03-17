9800a545fc52f6f310e92df4bc5f50002bea1820
oussamaabdulhay <oussama.abdulhay@gmail.com>
2021-03-17 16:52:10 +0400

    .

--- example_node/traslation_node.cpp
+++ example_node/traslation_node.cpp
@@ -131,7 +131,7 @@ int main(int argc, char** argv) {
     MissionElement* initial_pose_waypoint = new SetRelativeWaypoint(0., 0., 0., 0.); //TODO: SetRelativeWaypoint needs substantial refactoring
 
    
-    MissionElement* takeoff_relative_waypoint = new SetRelativeWaypoint(0., 0., 2.0, 0.);
+    MissionElement* takeoff_relative_waypoint = new SetRelativeWaypoint(0., 0., 1.5, 0.);
 
     MissionElement* land_relative_waypoint = new SetRelativeWaypoint(0., 0., -2., 0.);
 
