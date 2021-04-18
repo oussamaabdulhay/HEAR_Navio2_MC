863610fbbab7c4259d30f55cb46a8f131a27c99b
oussamaabdulhay <oussama.abdulhay@gmail.com>
2021-04-18 16:57:26 +0400

    .

--- example_node/traslation_node.cpp
+++ example_node/traslation_node.cpp
@@ -267,13 +267,13 @@ int main(int argc, char** argv) {
     ((UpdateController*)update_controller_pid_yaw_rate)->pid_data.id = block_id::PID_YAW_RATE;
 
     #ifdef TRANSLATION_X_CAMERA
-    ((UpdateController*)update_controller_camera_hovering_pid_x)->pid_data.kp = 1.3293; //0.6552
+    ((UpdateController*)update_controller_camera_hovering_pid_x)->pid_data.kp = 1.4343; //0.6552
     ((UpdateController*)update_controller_camera_hovering_pid_x)->pid_data.ki = 0.0; 
-    ((UpdateController*)update_controller_camera_hovering_pid_x)->pid_data.kd = 0.6899; //0.4782
+    ((UpdateController*)update_controller_camera_hovering_pid_x)->pid_data.kd = 0.5662; //0.4782
     ((UpdateController*)update_controller_camera_hovering_pid_x)->pid_data.kdd = 0.0;
     ((UpdateController*)update_controller_camera_hovering_pid_x)->pid_data.anti_windup = 0;
     ((UpdateController*)update_controller_camera_hovering_pid_x)->pid_data.en_pv_derivation = 1;
-    ((UpdateController*)update_controller_camera_hovering_pid_x)->pid_data.dt = (float)1.0/60.0;
+    ((UpdateController*)update_controller_camera_hovering_pid_x)->pid_data.dt = (float)1.0/200.0;
     ((UpdateController*)update_controller_camera_hovering_pid_x)->pid_data.id = block_id::PID_Camera_X;
 
     // ((UpdateController*)update_controller_camera_tracking_pid_x)->pid_data.kp = 0.416; //0.4878
