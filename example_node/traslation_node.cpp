ea9ab6924a76c0dceca89d13fffca67e3ebf8018
oussamaabdulhay <oussama.abdulhay@gmail.com>
2021-04-27 14:57:58 +0400

    testing mrft x det

--- example_node/traslation_node.cpp
+++ example_node/traslation_node.cpp
@@ -267,9 +267,9 @@ int main(int argc, char** argv) {
     ((UpdateController*)update_controller_pid_yaw_rate)->pid_data.id = block_id::PID_YAW_RATE;
 
     #ifdef TRANSLATION_X_CAMERA
-    ((UpdateController*)update_controller_camera_hovering_pid_x)->pid_data.kp = 1.1418; //0.6552
+    ((UpdateController*)update_controller_camera_hovering_pid_x)->pid_data.kp = 0.9448; //0.6552
     ((UpdateController*)update_controller_camera_hovering_pid_x)->pid_data.ki = 0.0; 
-    ((UpdateController*)update_controller_camera_hovering_pid_x)->pid_data.kd = 0.5925; //0.4782
+    ((UpdateController*)update_controller_camera_hovering_pid_x)->pid_data.kd = 0.5232; //0.4782
     ((UpdateController*)update_controller_camera_hovering_pid_x)->pid_data.kdd = 0.0;
     ((UpdateController*)update_controller_camera_hovering_pid_x)->pid_data.anti_windup = 0;
     ((UpdateController*)update_controller_camera_hovering_pid_x)->pid_data.en_pv_derivation = 1;
